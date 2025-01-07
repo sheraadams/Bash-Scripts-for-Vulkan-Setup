c3065791878878849fa99b8b020e0ad7e514e774
Shera Adams <110789514+sheraadams@users.noreply.github.com>
2025-01-07 08:56:11 -0500

    Update vulkan_setup.sh

--- vulkan_setup.sh
+++ vulkan_setup.sh
@@ -17,19 +17,12 @@ export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
 # Save the zshrc file
 source ~/.zshrc
 
+# Note: On Apple Silicon, I found the json files in:
+#~/VulkanSDK/<version>/macOS/etc/vulkan/explicit_layer.d
+#~/VulkanSDK/<version>/macOS/etc/vulkan/icd.d
+
 # Check that Vulkan is set up
 vulkaninfo
 
 # This guide is helpful as well: 
 # https://vulkan.lunarg.com/doc/view/1.3.236.0/mac/getting_started.html
-
-#Install and Uninstall from Terminal
-
-#The new macOS installer application can also be run from the command line allowing for unattended/scripted installation and uninstallation. Navigate to the folder containing the InstallVulkan.app bundle, and execute the embedded application directly. You will need to use sudo to give the installer permission to access the system folders, and you must also provide the destination path for the SDK installation files.
-
-sudo ./InstallVulkan.app/Contents/MacOS/InstallVulkan --root "installation path" --accept-licenses --default-answer --confirm-command install
-
-#For example, to install into your home folder using a root of "VulkanSDK", and an SDK version of 1.3.224.1, including all optional packages, you would use this command:
-
-sudo ./InstallVulkan.app/Contents/MacOS/InstallVulkan --root ~/VulkanSDK/1.2.176.1 --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.core com.lunarg.vulkan.usr com.lunarg.vulkan.sdl2 com.lunarg.vulkan.glm com.lunarg.vulkan.volk com.lunarg.vulkan.vma
-
